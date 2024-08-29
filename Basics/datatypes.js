66d70c9e51388483bb0bb853f83fce50c08023a8
sarvesh7999 <sarveshgupta50@gmail.com>
2024-08-29 05:50:57 +0000

    variables

---
+++ Basics/datatypes.js
@@ -0,0 +1,20 @@
+// Datatypes are of basically 2 types - 
+
+// Primitive and Non-primitive
+
+// Primitive have following data types -- 
+
+// 1. String -- "", ''
+// 2. Number -- 2^53, -, +, decimal
+// 3. Boolean -- true, false
+// 4. BigInt -- bigger than 2^53
+// 5. null -- unique value
+// 6. undefined -- which is not defined
+// 7. Symbol
+
+
+// Non primitive data types -- 
+
+// 1. Array
+// 2. Object
+// 3. Functions
---
+++ Basics/variables.js
@@ -0,0 +1,30 @@
+// Variables are memory spaces which contain some value and are referred in the code
+
+// To define a variable we have 3 wways -- 
+// 1. var - not used anymore
+// 2. let
+// 3. const
+
+// Let -- 
+
+let firstName = "Avinash"
+
+console.log(firstName) // To display things on terminal. To use this file use "node filenmae"
+
+firstName = "Sarvesh"
+
+console.log(firstName)
+
+// const -- 
+
+const lastName = "Dubey"
+
+console.log(lastName)
+
+lastName  = "Sharma" // Not allowed
+
+console.log(lastName)
+
+// Difference between let and const -- 
+
+// let will allow you to change the variable values but const is a constant value than can never be changed.
